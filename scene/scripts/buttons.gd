328498e9e83421d027b458703079b1a3be20e19c
brunojardon <brunojardon3@gmail.com>
2021-10-18 18:53:05 -0300

    comvinacion de journey

--- scene/scripts/buttons.gd
+++ scene/scripts/buttons.gd
@@ -4,18 +4,20 @@ extends Control
 func _ready():
 	pass
 
+func _on_journeyButton_pressed():
+	get_tree().change_scene("res://scene/world.tscn")
+	print("Journey")
+
 func _on_playButton_pressed():
 	get_tree().change_scene("res://scene/battleScene.tscn")
 	print("Jugar")
-
-func _on_journeyButton_pressed():
-	get_tree().change_scene("res://scene/journeyScene.tscn")
 
 func _on_optionsButton_pressed():
 	get_tree().change_scene("res://scene/configScene.tscn")
 	print("Configuracion")
 
-
 func _on_exitButton_pressed():
 	get_tree().quit()
 	print("Cerrar")
+
+
