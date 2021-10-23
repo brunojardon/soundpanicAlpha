657b9c1304a45b341a4866d1335f79115fe2beae
brunojardon <brunojardon3@gmail.com>
2021-10-23 18:23:51 -0300

    Texturas para animaciones agregadas

--- scene/scripts/battleScene.gd
+++ scene/scripts/battleScene.gd
@@ -1,13 +1,12 @@
 extends Node2D
 
-var proyectil = preload("res://scene/proyectil.tscn")
+#var proyectil = preload("res://scene/proyectil.tscn")
 
 func _ready():
 	OS.set_window_title("Mecanica") #Coloca el titulo de ventana
 	
 
-func _on_Timer_timeout():
-	var new_proyectil = proyectil.instance()
-	new_proyectil.position = Vector2(856.642,479.98)
-	add_child(new_proyectil)
- # Replace with function body.
+#func _on_Timer_timeout():
+#	var new_proyectil = proyectil.instance()
+#	new_proyectil.position = Vector2(856.642,479.98)
+#	add_child(new_proyectil)
--- scene/scripts/proyectil.gd
+++ scene/scripts/proyectil.gd
@@ -3,23 +3,30 @@ extends Area2D
 var movimiento = Vector2()
 var dentro = false #Indica si el proyectil esta dentro o fuera del aro
 export var salud = 300 #Vida restante del personaje
-export var missKey = 50 #Daño por fallar
+export var missKey = 400 #Daño por fallar
 
 func _on_aro_area_shape_entered(area_id, area, area_shape, local_shape):
 	dentro = true #Proyectil dentro de aro
 
 func _process(delta): #Se ejecuta cada frame
-	movimiento.x = -200 #Aumenta -200 en su eje X
-	position += movimiento * delta
+	print(dentro)
+	if Input.is_action_pressed("key-E"):
+		movimiento.x = -200 #Aumenta -200 en su eje X
+		position += movimiento * delta
 	if Input.is_action_just_pressed("key-A") and dentro==true: #Si se presiona "A" y el proyectil esta dentro...
-		salud=salud+10 #Se suma 10 de salud
-		print(salud) 
+		salud = salud + 10 #Se suma 10 de salud
 		queue_free() #Se elimina el proyectil
+	else:
+		pass
 
 func _on_aro_area_shape_exited(area_id, area, area_shape, local_shape):
 	dentro = false #Proyectil fuera de aro
 
-func _on_protaArea_area_entered(area): #Proyectil colisiona con prota
+#func _on_protaArea_area_entered(area): #Proyectil colisiona con prota
+#	salud = salud - missKey #Se resta el fallo con la salud
+#	queue_free() #Se elimina el proyectil <------------------------------------------------- mal
+
+
+func _on_protaArea_area_shape_entered(area_id, area, area_shape, local_shape):
 	salud = salud - missKey #Se resta el fallo con la salud
-	print(salud)
 	queue_free() #Se elimina el proyectil
